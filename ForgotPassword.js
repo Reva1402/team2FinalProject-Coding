899bf186a76335b5d24e1057a089813fc0d7e870
Reva1402 <148901721+Reva1402@users.noreply.github.com>
2024-11-19 11:02:50 -0500

    Update ForgotPassword.js

--- ForgotPassword.js
+++ ForgotPassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import { auth } from './firebaseConfig';  // Make sure this is configured correctly
+import { auth } from './firebaseConfig';  
 import './ForgotPassword.css';
 
 const ForgotPassword = () => {
